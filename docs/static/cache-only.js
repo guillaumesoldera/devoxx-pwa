f622aea529c378f5691b61eb274f8b53725eb7e5
guillaumesoldera <guillaumesoldera@gmail.com>
2018-10-21 19:07:28 +0200

    add cache only

---
+++ docs/static/cache-only.js
@@ -0,0 +1,41 @@
+'use strict';
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('devoxx-pwa-cache-assets').then(function(cache) {
+        console.log('caching assets');
+        return cache.addAll([
+          '../assets/css/style.css',
+          '../assets/images/devoxx_brand.gif',
+          '../assets/images/cache-only.png',
+          './cache-only.html'
+        ]);
+      })
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    // If a match isn't found in the cache, the response
+    // will look like a connection error
+    event.respondWith(function(){
+      var cacheResponse = caches.match(event.request)
+      if (cacheResponse) {
+        console.log('element found in cache for ', event.request)
+      } else {
+        console.log('element not found in cache for ', event.request)
+      }
+      return cacheResponse;
+    }());
+  });
