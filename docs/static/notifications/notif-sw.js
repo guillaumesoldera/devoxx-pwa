865b70aea53f96142396a02c9482ddbd0aa7e242
guillaumesoldera <guillaumesoldera@gmail.com>
2018-10-23 17:24:25 +0200

    set icon for notification

--- docs/static/notifications/notif-sw.js
+++ docs/static/notifications/notif-sw.js
@@ -1,1 +1,15 @@
 self.addEventListener('fetch', function(event) {})
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('On notification click: ', event.notification);
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        }).then(function () {
+            if (clients.openWindow) {
+                return clients.openWindow(`http://guillaumesoldera.github.io/devoxx-pwa/static/notifications/notifications.html?time=${event.notification.data.time}`);
+            }
+        })
+    );
+});
