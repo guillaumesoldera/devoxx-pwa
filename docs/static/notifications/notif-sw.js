a49f7de41025142b75d6a2858a3ba4f2ad153f4a
guillaumesoldera <guillaumesoldera@gmail.com>
2018-10-24 10:59:56 +0200

    open notif in current tab if it's already open

--- docs/static/notifications/notif-sw.js
+++ docs/static/notifications/notif-sw.js
@@ -3,13 +3,38 @@ self.addEventListener('fetch', function(event) {})
 self.addEventListener('notificationclick', function (event) {
     console.log('On notification click: ', event.notification);
     event.notification.close();
-    event.waitUntil(
-        clients.matchAll({
-            type: "window"
-        }).then(function () {
-            if (clients.openWindow) {
-                return clients.openWindow(`http://guillaumesoldera.github.io/devoxx-pwa/static/notifications/notifications.html?time=${event.notification.data.time}`);
-            }
-        })
-    );
+    // open in new window
+    //event.waitUntil(
+    //    clients.matchAll({
+    //        type: "window"
+    //    }).then(function () {
+    //        if (clients.openWindow) {
+    //            return clients.openWindow(`/static/notifications/notifications.html?time=${event.notification.data.time}`);
+    //        }
+    //    })
+    //);
+    const urlToOpen = new URL(`/static/notifications/notifications.html`, self.location.origin).href;
+    const promiseChain = clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+      })
+      .then((windowClients) => {
+        let matchingClient = null;
+    
+        for (let i = 0; i < windowClients.length; i++) {
+          const windowClient = windowClients[i];
+          if (windowClient.url === urlToOpen) {
+            matchingClient = windowClient;
+            break;
+          }
+        }
+    
+        if (matchingClient) {
+          return matchingClient.focus();
+        } else {
+          return clients.openWindow(urlToOpen);
+        }
+      });
+    
+      event.waitUntil(promiseChain);
 });
